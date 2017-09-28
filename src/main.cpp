815f10d6b9db5013c6a53de65db5d4d5c1620e78
Timothy Finucane <timfinucane@outlook.com>
2017-09-28 23:02:49 +1300

    Test basic use of SyntaxFormer

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,82 @@
-#include <fstream>
 #include <iostream>
-#include <time.h>
+
+#include "syntax/SyntaxFormer.h"
+
+const unsigned int SENTENCE = 0;
+const unsigned int VERB = 1;
+const unsigned int ADJECTIVE = 2;
+const unsigned int NOUN = 3;
+const unsigned int NOUN_PHRASE = 4;
+const unsigned int ARTICLE = 5;
 
 int main( int argc, char* argv[] )
 {
+    SyntaxFormer former;
+
+    former.addSymbol( SENTENCE, 0, false );
+    former.addSymbol( VERB, 0, true );
+    former.addSymbol( ADJECTIVE, 0, true );
+    former.addSymbol( NOUN, 0, true );
+    former.addSymbol( NOUN_PHRASE, 0, false );
+    former.addSymbol( ARTICLE, 0, true );
+
+    SyntaxFormer::Production production;
+    // Sentence A
+    production.add( NOUN_PHRASE, {} );
+    production.add( VERB, {} );
+
+    former.addProduction( SENTENCE, std::move( production ) );
+    production = SyntaxFormer::Production();
+
+    // Sentence B
+    production.add( ARTICLE, {} );
+    production.add( NOUN_PHRASE, {} );
+    production.add( VERB, {} );
+
+    former.addProduction( SENTENCE, std::move( production ) );
+    production = SyntaxFormer::Production();
+
+    // Noun phrase A
+    production.add( ADJECTIVE, {} );
+    production.add( NOUN_PHRASE, {} );
+    
+    former.addProduction( NOUN_PHRASE, std::move( production ) );
+    production = SyntaxFormer::Production();
+
+    // Noun phrase B
+    production.add( NOUN, {} );
+
+    former.addProduction( NOUN_PHRASE, std::move( production ) );
+    production = SyntaxFormer::Production();
+
+    for( auto symbol : former.generate( SENTENCE ) )
+    {
+        switch( symbol.symbol )
+        {
+        case VERB:
+            std::cout << "Verb";
+            break;
+        case ADJECTIVE:
+            std::cout << "Adjective";
+            break;
+        case NOUN:
+            std::cout << "Noun";
+            break;
+        case ARTICLE:
+            std::cout << "Article";
+            break;
+        default:
+            std::cout << "Invalid";
+            break;
+        }
+
+        std::cout << " ";
+    }
+
+    std::cout << std::endl;
+
+    char c;
+    std::cin >> c;
+
     return 0;
 }
