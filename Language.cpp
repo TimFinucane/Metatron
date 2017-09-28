257701959f0038ef1feafe4be36061678507f33a
Timothy Finucane <timfinucane@outlook.com>
2017-09-28 17:54:43 +1300

    Completely restructure project. Delete most things

--- Language.cpp
+++
@@ -1,8 +0,0 @@
-#include "Language.h"
-
-using namespace Language;
-
-bool Language::isVowel( char c )
-{
-    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-}
---
+++ src/generation/Generation.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "syntax/Syntax.h"
+
+namespace Syntax
+{
+}
