703a6474ffc5e64288d15bd863e5332fd2d91c78
Timothy Finucane <timfinucane@outlook.com>
2017-09-28 23:01:57 +1300

    Fix bug in SyntaxFormer

--- src/syntax/SyntaxFormer.cpp
+++ src/syntax/SyntaxFormer.cpp
@@ -17,10 +17,11 @@ std::list<SyntaxFormer::OutSymbol>  SyntaxFormer::generate( unsigned int symbol 
         {
             // Choose a random production. TODO: Allow input chances?
             auto prodsPair = productions.equal_range( outputIt->symbol );
-            std::uniform_int<> uniform( 0, (int)std::distance( prodsPair.first, prodsPair.second ) );
+            std::uniform_int<> uniform( 0, (int)std::distance( prodsPair.first, prodsPair.second ) - 1 );
             auto chosen = std::next( prodsPair.first, uniform( engine ) );
             
             // Add all the production's symbols
+            auto prevIt = outputIt;
             for( auto symbol : chosen->second.symbols )
             {
                 std::vector<unsigned int> symbolArgs( symbols[symbol.first].argumentCount, 0 );
@@ -33,13 +34,13 @@ std::list<SyntaxFormer::OutSymbol>  SyntaxFormer::generate( unsigned int symbol 
                         symbolArgs[arg.first] = arg.second.value + outputIt->args[arg.second.headArg];
                 }
 
-                output.insert( outputIt, OutSymbol{ symbol.first, symbolArgs } );
+                prevIt = output.insert( outputIt, OutSymbol{ symbol.first, symbolArgs } );
             }
 
             // Erase, and restart loop
             output.erase( outputIt );
             outputIt = output.begin();
-            break;
+            continue;
         }
         
         outputIt++;
