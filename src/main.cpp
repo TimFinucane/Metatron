52f2ffb36a2af894c5d58272e0be47c1b454b18c
Timothy Finucane <timfinucane@outlook.com>
2017-10-04 21:19:14 +1300

    Produce example code for using Grammar

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
+#include <string>
 
 #include "grammar/Grammar.h"
 
-const unsigned int SENTENCE = 0;
-const unsigned int VERB = 1;
-const unsigned int ADJECTIVE = 2;
-const unsigned int NOUN = 3;
-const unsigned int NOUN_PHRASE = 4;
-const unsigned int ARTICLE = 5;
+enum SymbolType : unsigned int
+{
+    SENTENCE,
+    VERB,
+    ADJECTIVE,
+    NOUN,
+    NOUN_PHRASE,
+    ARTICLE,
+    ADJECTIVE_SEQUENCE
+};
+
+std::map<unsigned int, std::string> names
+{
+    { VERB, "verb" },
+    { ADJECTIVE, "adjective" },
+    { NOUN, "noun" },
+    { ARTICLE, "article" }
+};
 
 int main( int argc, char* argv[] )
 {
+    using namespace Grammar;
+
+    Former former;
+
+    former.addProduction( SENTENCE, Production( { NOUN_PHRASE, VERB } ) );
+    former.addProduction( SENTENCE, Production( { NOUN_PHRASE, VERB, NOUN_PHRASE } ) );
+    former.addProduction( NOUN_PHRASE, Production( { ARTICLE, ADJECTIVE_SEQUENCE, NOUN } ) );
+    former.addProduction( NOUN_PHRASE, Production( { ADJECTIVE_SEQUENCE, NOUN } ) );
+    former.addProduction( NOUN_PHRASE, Production( { NOUN } ) );
+    former.addProduction( ADJECTIVE_SEQUENCE, Production( { ADJECTIVE } ) );
+    former.addProduction( ADJECTIVE_SEQUENCE, Production( { ADJECTIVE, ADJECTIVE_SEQUENCE } ) );
+
+    String output = former.generate( SENTENCE );
+
+    for( const Symbol& symbol : output.symbols() )
+    {
+        std::cout << names[symbol.id] << " ";
+    }
+    std::cout << std::endl;
+
     char c;
     std::cin >> c;
 
